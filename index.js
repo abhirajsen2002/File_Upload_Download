d837c24546b42fcdb39ef03ed8082b3115e11526
Abhiraj Sen <80464878+abhirajsen2002@users.noreply.github.com>
2023-02-06 15:44:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, (file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+});
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended : true }))
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/public/test.html")
+})
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.send("File uploaded successfully");
+});
+
+app.post("/download", (req, res) => {
+  res.download("uploads/" + req.body.filename);
+  console.log(req.body)
+});
+
+// app.get("/view/:filename", (req, res) => {
+//   res.sendFile("public/uploads/" + req.params.filename, {
+//     root: __dirname
+//   });
+// });
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
